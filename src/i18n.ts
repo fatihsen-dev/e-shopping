7d2aac7e0ad90250a03f42dce2089e179f564307
Fatih Şen <fatihydrm@outlook.com>
2023-02-20 02:25:00 +0300

    edit: get user language from localstorage

--- src/i18n.ts
+++ src/i18n.ts
@@ -14,7 +14,7 @@ const resources = {
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: "en",
+  lng: localStorage.getItem("lang") || "en",
   interpolation: {
     escapeValue: false,
   },

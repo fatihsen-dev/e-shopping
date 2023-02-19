a5e087ac3b8b9f656dff745618ebc693e0b9c7b2
Fatih Şen <fatihydrm@outlook.com>
2023-02-20 02:25:24 +0300

    add: Router system added

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import "./styles/reset.scss"
+import "./styles/scss/index.scss"
 import "./i18n"
 import { Provider } from "react-redux"
 import { store } from "./stores"
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 )

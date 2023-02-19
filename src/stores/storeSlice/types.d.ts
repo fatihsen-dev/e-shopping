5a655a8f51f37230784f4d5f0c9edf1ec8ff7034
Fatih Şen <fatihydrm@outlook.com>
2023-02-20 02:39:05 +0300

    create:  create and configure store types

--- src/stores/storeSlice/types.d.ts
+++ src/stores/storeSlice/types.d.ts
@@ -1,3 +1,34 @@
-interface StoreState {
-  value: number
+type Product = {
+  id: number
+  title: string
+  description: string
+  price: number
+  discountPercentage: number
+  rating: number
+  stock: number
+  brand: string
+  category: string
+  thumbnail: string
+  images: [string]
 }
+
+type Favorites = {
+  favorites: Product[]
+}
+
+type cartExtendedType = Product & { count: number }
+
+type Cart = {
+  cart: cartExtendedType[]
+}
+
+type Products = {
+  products: {
+    products: Product[]
+    total: number
+    skip: number
+    limit: number
+  }
+}
+
+type StoreState = Favorites & Products & Cart

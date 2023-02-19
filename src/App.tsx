028e798ff506ebc7f0ac30543a02ec59f3ebd642
Fatih Şen <fatihydrm@outlook.com>
2023-02-20 02:23:27 +0300

    edit: added router system and added error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,68 @@
-import React, { useEffect, useState } from "react"
-import { useTranslation } from "react-i18next"
+import React, { useEffect } from "react"
+import styles from "./App.module.scss"
+import Navbar from "./components/Navbar/Navbar"
+import { useAppDispatch, useAppSelector } from "./stores/hook"
+import { Route, Routes } from "react-router-dom"
+import Products from "./pages/Products"
+import Favorites from "./pages/Favorites"
+import Cart from "./pages/Cart"
+import Account from "./pages/Account/Account"
+import { requestAllProducts } from "./axios"
+import { loadAllProducts } from "./stores/storeSlice/shopSlice"
+import { changeStatus } from "./stores/siteSlice/siteSlice"
+import Error from "./pages/Error"
+import { Toaster } from "react-hot-toast"
 
 export default function App() {
-  const { t, i18n } = useTranslation()
+  const { darkMode, status } = useAppSelector((state) => state.site)
+  const dispatch = useAppDispatch()
 
-  return (
-    <div>
-      <h1>{t("welcome")}</h1>
-      <button onClick={() => i18n.changeLanguage(i18n.language == "tr" ? "en" : "tr")}>Change</button>
-    </div>
-  )
+  useEffect(() => {
+    darkMode ? document.body.classList.add("dark") : document.body.classList.remove("dark")
+  }, [darkMode])
+
+  useEffect(() => {
+    getAllProducts()
+  }, [])
+
+  const getAllProducts = async () => {
+    try {
+      const response = await requestAllProducts()
+      dispatch(loadAllProducts({ products: response.data }))
+      dispatch(changeStatus(true))
+    } catch (error) {
+      console.log(error)
+      dispatch(changeStatus(false))
+    }
+  }
+
+  if (status === true)
+    return (
+      <>
+        <div className={styles.app}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+        </div>
+        <Toaster position="top-right" reverseOrder={false} />
+      </>
+    )
+  if (status === false)
+    return (
+      <>
+        <Error />
+        <Toaster position="top-right" reverseOrder={false} />
+      </>
+    )
+  if (status === null)
+    return (
+      <>
+        <div />
+        <Toaster position="top-right" reverseOrder={false} />
+      </>
+    )
 }
